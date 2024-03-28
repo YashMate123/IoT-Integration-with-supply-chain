df2c47235f5995bee8842d196526f35e02fe923e
Yash Mate <yashrmate.iitkgp@gmail.com>
2024-03-28 14:50:20 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './components/navbar/navbar'
+import MainComponent from './components/MainComponent'
+import FormComponent from './components/FormComponent'
+
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      {/* <MainComponent /> */}
+      <FormComponent />
+    </>
+  )
+}
